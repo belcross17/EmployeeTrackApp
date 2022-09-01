e7e8fd0b7e394552482a9c2f5d590eb337548ebb
Belcross17 <alfredochabes1@hotmail.com>
2022-09-01 16:54:08 -0400

    created tables for roles and employee

--- db/schema.sql
+++ db/schema.sql
@@ -3,8 +3,25 @@ CREATE DATABASE brayan_db;
 USE brayan_db;
 
 DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS employee;
 
 CREATE TABLE department (
   id INT PRIMARY KEY AUTO_INCREMENT,
   dept_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT
+);
+
+CREATE TABLE employee (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    job_id INT,
+    manager_id INT
 );
